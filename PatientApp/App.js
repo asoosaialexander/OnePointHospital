517059715fde4bc293ee2e230ae473193e6758cc
Soosai Alexander <asoosaialexander@gmail.com>
2021-04-06 13:48:26 +0530

    Patient App: Added Search Doctor Page

--- PatientApp/App.js
+++ PatientApp/App.js
@@ -1,37 +1,60 @@
-import * as React from 'react'
-import {Text} from 'react-native'
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import { NavigationContainer } from '@react-navigation/native';
+import * as React from 'react';
+import HomeScreen from './Views/HomeScreen';
+import {Button, View} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Header, Icon} from 'react-native-elements';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-// function HomeScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Button
-//         onPress={() => navigation.navigate('Notifications')}
-//         title="Go to notifications"
-//       />
-//     </View>
-//   );
-// }
-
-// function NotificationsScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Button onPress={() => navigation.goBack()} title="Go back home" />
-//     </View>
-//   );
-// }
-
-// const Drawer = createDrawerNavigator();
-
-export default function App () {
+function MyTabs() {
   return (
-    // <NavigationContainer>
-    //   <Drawer.Navigator initialRouteName="Home">
-    //     <Drawer.Screen name="Home" component={HomeScreen} />
-    //     <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-    //   </Drawer.Navigator>
-    // </NavigationContainer>
-    <Text>Patient Application</Text>
-  )
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'reader' : 'reader-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={NotificationsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+function NotificationsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={MyTabs} />
+          <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
 }
---
+++ PatientApp/Views/HomeScreen.js
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import {
+  Avatar,
+  Header,
+  Icon,
+  SearchBar,
+  Text,
+  Button,
+  ListItem,
+} from 'react-native-elements';
+
+const list = [
+  {
+    name: 'Dr. Rajesh',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Pediatrician',
+  },
+  {
+    name: 'Dr. Sadique Basha',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Pediatrician',
+  },
+];
+
+class HomeScreen extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = search => {
+    this.setState({search});
+  };
+
+  getInitials = name => {
+    let names = name.split(' ');
+    let initials = '';
+    names.forEach(name => {
+      if (name.toLowerCase() != 'dr.' && name.toLowerCase() != 'dr') {
+        initials += name.substring(0, 1).toUpperCase();
+      }
+    });
+    return initials;
+  };
+
+  render() {
+    const {search} = this.state;
+
+    return (
+      <>
+        <Header
+          leftComponent={
+            <Icon
+              name="menu"
+              type="material"
+              onPress={() => this.props.navigation.toggleDrawer()}
+            />
+          }
+          centerComponent={{text: 'Patient Log', style: {color: '#fff'}}}
+          rightComponent={{icon: 'home', color: '#fff'}}
+        />
+        <View style={{margin: 5}}>
+          <Text h3>Find Your Doctor</Text>
+          <Text h6>Book and appointment for consulation</Text>
+          <SearchBar
+            placeholder="Type Here..."
+            onChangeText={this.updateSearch}
+            value={search}
+            platform="android"
+          />
+        </View>
+        {search != '' && (
+          <View style={{padding: 3, paddingLeft: 20}}>
+            <Text>Search results for {search}</Text>
+          </View>
+        )}
+        <View>
+          {list.map((l, i) => (
+            <ListItem key={i} bottomDivider>
+              <Avatar
+                rounded
+                title={this.getInitials(l.name)}
+                source={{uri: l.avatar_url}}
+              />
+              <ListItem.Content>
+                <ListItem.Title>{l.name}</ListItem.Title>
+                <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          ))}
+        </View>
+      </>
+    );
+  }
+}
+
+export default HomeScreen;
